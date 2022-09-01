ebd9491aefc9f6b5c2c903fac2418e1de7fa4d2e
vhenaoi <vhenao.isaza@gmail.com>
2022-09-01 23:33:06 +0200

    solo para ensayar

---
+++ Manipulacion- Rois-Componentes de todas las DB/Datosparaorganizardataframes/lecturafiltrado.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+import numpy as np
+
+BasesdeDatosFiltradas=pd.read_feather(r'Datosparaorganizardataframes\BasesdeDatosFiltradas_componenteporcolumnas.feather')
+print(BasesdeDatosFiltradas)
