2dce459d7d38fee9d318c56b189fed9893e700f2
valeriacadavidc <valeria.cadavidc@udea.edu.co>
2022-09-22 18:40:11 -0500

    "Resultados de las pruebas estadisticas de diferencias grupales en un archivo csv"

--- Manipulacion- Rois-Componentes de todas las DB/Pruebas_estadisticas.py
+++ Manipulacion- Rois-Componentes de todas las DB/Pruebas_estadisticas.py
@@ -58,7 +58,7 @@ def normality_Agostino(data,name_data):
 
 #Test de homocedasticidad
 
-def normality_N_groups_Shapiro(data,dv,group):
+def homocedasticidad_N_groups_Shapiro(data,dv,group):
     '''
     Test equality of variance
 
@@ -90,6 +90,9 @@ def normality_N_groups_Shapiro(data,dv,group):
 #This function makes  two-by-two comparisons  which do not require the groups to have equal variances. It is non-parametric
 #pg.pairwise_gameshowell(data=df, dv='name where are the values', between='groups to compare')
 
+#Some theory https://statisticsbyjim.com/anova/welchs-anova-compared-to-classic-one-way-anova/
+
+## Pruebas datos componentes
 
 icc=['C14_rDelta', 'C14_rTheta', 'C14_rAlpha-1', 'C14_rAlpha-2',
        'C14_rBeta1', 'C14_rBeta2', 'C14_rBeta3', 'C14_rGamma', 'C15_rDelta',
@@ -106,19 +109,112 @@ icc=['C14_rDelta', 'C14_rTheta', 'C14_rAlpha-1', 'C14_rAlpha-2',
        'C24_rBeta3', 'C24_rGamma', 'C25_rDelta', 'C25_rTheta', 'C25_rAlpha-1',
        'C25_rAlpha-2', 'C25_rBeta1', 'C25_rBeta2', 'C25_rBeta3']
 
+bandas=['Delta','Theta','Alpha-1','Alpha-2','Beta1','Beta2','Beta3','Gamma']
+componentes=['C14', 'C15','C18', 'C20', 'C22','C23', 'C24', 'C25']
 datos=pd.read_feather(r'Manipulacion- Rois-Componentes de todas las DB\Datosparaorganizardataframes\BasesdeDatosFiltradas_componenteporcolumnas.feather')
 datos=datos.drop(columns='index')
 datos['index'] = datos.index
 
-for i in icc:
-    print('\nDatos de '+i)
-    if(datos[i].isnull().tolist()[0]==False):
-        print(pg.normality(data=datos, dv=i, group='database',method='shapiro'))
+groups=['Control', 'DTA']
+resultados_comparacion = pd.DataFrame(columns=['Group','Component','Band','p-unc','Result','Databases evaluated'])
+for j in groups:
+    print('\nGrupo '+j)
+    for i in icc:
+        print('\nDatos de '+i)
+        for k in bandas:
+            if k in i:
+                band=k
+        for c in componentes:
+            if c in i:
+                componente=c
+        if(datos[i].isnull().tolist()[0]==False):
+            print('Normalidad')
+            print(pg.normality(data=datos[datos['group'].isin([j])], dv=i, group='database',method='shapiro'))
+            print('Varianza')
+            print(pg.homoscedasticity(data=datos[datos['group'].isin([j])], dv=i, group='database',method='bartlett'))
+            #EN general no estan dando con varianza igual
+            print('Welch ANOVA')
+            welch=pg.welch_anova(data=datos[datos['group'].isin([j])], dv=i, between='database')
+            print(welch)
+            valor=welch['p-unc'][0]
+            if valor<0.05:
+                print('Hay diferencias estadisticamente significativas entre las medias')
+                print('No Comparables')
+                result='group differences'
+            else:
+                print('No hay diferencias estadisticamente significativas entre las medias')
+                print('Comparables')
+                result='No group differences'
 
+            print('Gameshowell test')
+            a=pg.pairwise_gameshowell(data=datos[datos['group'].isin([j])], dv=i, between='database')
+            print(a)
+            databases=datos[datos['group'].isin(['Control'])]['database'].unique()
+            b=a['A'].unique().tolist()+a['B'].unique().tolist()
+            b=set(b)
+            listToStr = ','.join([str(elem) for elem in b])
+            nueva_fila = { 'Group':j,'Component':componente,'Band':band,'p-unc':valor,'Result':result,'Databases evaluated':listToStr} # creamos un diccionario
+            resultados_comparacion = resultados_comparacion.append(nueva_fila, ignore_index=True)
+resultados_comparacion.to_csv('Manipulacion- Rois-Componentes de todas las DB\Datosparaorganizardataframes\ResultadosWelchComponentes.csv',sep=';',index=False)
 
-print(pg.welch_anova(data=datos, dv=i, between='database'))
-pg.pairwise_gameshowell(data=datos[datos['group']=='Control'], dv='C14_rDelta', between='database')
+##Pruebas para los ROIs
+datos=pd.read_feather(r'Manipulacion- Rois-Componentes de todas las DB\Datosparaorganizardataframes\BasesdeDatosFiltradas_ROIporcolumnas.feather')
+datos=datos.drop(columns='index')
+datos['index'] = datos.index
 
-pg.welch_anova(data=datos, dv='C14_rDelta', between='database') 
+rois=['ROI_F_rDelta','ROI_C_rDelta', 'ROI_PO_rDelta', 'ROI_T_rDelta', 'ROI_F_rTheta',
+       'ROI_C_rTheta', 'ROI_PO_rTheta', 'ROI_T_rTheta', 'ROI_F_rAlpha-1',
+       'ROI_C_rAlpha-1', 'ROI_PO_rAlpha-1', 'ROI_T_rAlpha-1', 'ROI_F_rAlpha-2',
+       'ROI_C_rAlpha-2', 'ROI_PO_rAlpha-2', 'ROI_T_rAlpha-2', 'ROI_F_rBeta1',
+       'ROI_C_rBeta1', 'ROI_PO_rBeta1', 'ROI_T_rBeta1', 'ROI_F_rBeta2',
+       'ROI_C_rBeta2', 'ROI_PO_rBeta2', 'ROI_T_rBeta2', 'ROI_F_rBeta3',
+       'ROI_C_rBeta3', 'ROI_PO_rBeta3', 'ROI_T_rBeta3', 'ROI_F_rGamma',
+       'ROI_C_rGamma', 'ROI_PO_rGamma', 'ROI_T_rGamma']
+roi=['ROI_F', 'ROI_C','ROI_PO', 'ROI_T']
+
+resultados_comparacion = pd.DataFrame(columns=['Group','ROI','Band','p-unc','Result','Databases evaluated'])
+for j in groups:
+    print('\nGrupo '+j)
+    for i in rois:
+        print('\nDatos de '+i)
+        for k in bandas:
+            if k in i:
+                band=k
+        for c in roi:
+            if c in i:
+                r=c
+        if(datos[i].isnull().tolist()[0]==False):
+            print('Normalidad')
+            print(pg.normality(data=datos[datos['group'].isin([j])], dv=i, group='database',method='shapiro'))
+            print('Varianza')
+            print(pg.homoscedasticity(data=datos[datos['group'].isin([j])], dv=i, group='database',method='bartlett'))
+            #EN general no estan dando con varianza igual
+            print('Welch ANOVA')
+            welch=pg.welch_anova(data=datos[datos['group'].isin([j])], dv=i, between='database')
+            print(welch)
+            valor=welch['p-unc'][0]
+            if valor<0.05:
+                print('Hay diferencias estadisticamente significativas entre las medias')
+                print('No Comparables')
+                result='group differences'
+            else:
+                print('No hay diferencias estadisticamente significativas entre las medias')
+                print('Comparables')
+                result='No group differences'
+
+            print('Gameshowell test')
+            a=pg.pairwise_gameshowell(data=datos[datos['group'].isin([j])], dv=i, between='database')
+            print(a)
+            databases=datos[datos['group'].isin(['Control'])]['database'].unique()
+            b=a['A'].unique().tolist()+a['B'].unique().tolist()
+            b=set(b)
+            listToStr = ','.join([str(elem) for elem in b])
+            nueva_fila = { 'Group':j,'ROI':r,'Band':band,'p-unc':valor,'Result':result,'Databases evaluated':listToStr} # creamos un diccionario
+            resultados_comparacion = resultados_comparacion.append(nueva_fila, ignore_index=True)
+resultados_comparacion.to_csv('Manipulacion- Rois-Componentes de todas las DB\Datosparaorganizardataframes\ResultadosWelchROIs.csv',sep=';',index=False)
+# print(pg.welch_anova(data=datos, dv=i, between='database'))
+# pg.pairwise_gameshowell(data=datos[datos['group']=='Control'], dv='C14_rDelta', between='database')
+
+# pg.welch_anova(data=datos, dv='C14_rDelta', between='database') 
 
 # Comparaciones múltiples
