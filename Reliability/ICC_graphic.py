ac17fdc5b7da08ded7ac5fea3be04ab66009327b
vhenaoi <vhenao.isaza@gmail.com>
2022-09-23 22:19:19 +0200

    actualizar

---
+++ Reliability/ICC_graphic.py
@@ -0,0 +1,114 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+
+icc_data_Roi=pd.read_csv(r'Reliability\ICC_values_csv\icc_values_ROIS_G2-CTR.csv',sep=';')
+icc_data_Comp=pd.read_csv(r'Reliability\ICC_values_csv\icc_values_Components_G2-CTR.csv',sep=';')
+
+#Para cambiar los nombres de los grupos en las graficas del articulo
+# icc_data_Roi['Group'].replace({'G2':'Group 2','CTR':'Group 1'}, inplace=True)
+# icc_data_Comp['Group'].replace({'G2':'Group 2','CTR':'Group 1'}, inplace=True)
+
+def barplot_icc_nB_1G(icc_data,x_value,group,plot=False,save=False):
+    filter_band=icc_data[icc_data['Group']==group]
+    sns.set(font_scale = 0.9)
+    sns.set_theme(style="white")
+    ax=sns.catplot(x=x_value,y='ICC',data=filter_band,hue='Stage',palette='winter_r',kind='bar',col='Bands',col_wrap=4,legend=False)
+    ax.fig.suptitle('ICC3k for '+ x_value +' in frequency bands of '+group+' group')
+    ax.add_legend(loc='upper center',bbox_to_anchor=(.5,0.94),ncol=2)
+    ax.fig.subplots_adjust(top=0.85,bottom=0.133, right=0.936,left=0.062, hspace=0.143, wspace=0.11) # adjust the Figure in rp
+    ax.set(xlabel=None)
+    ax.set(ylabel=None)
+    ax.fig.text(0.5, 0.07, x_value, ha='center', va='center')
+    ax.fig.text(0.03, 0.5,  'ICC', ha='center', va='center',rotation='vertical')
+    if save==True:
+        plt.savefig('Reliability\ICC_Graphics\ICC_{name_group}_{tipo}.png'.format(name_group=group,tipo=x_value))
+        plt.close()
+    if plot:
+        plt.show()
+
+def barplot_icc_comp_nG(icc_data,x_value,title,plot=False,save=False):
+    sns.set(font_scale = 0.9)
+    sns.set_theme(style="white")
+    ax=sns.catplot(x=x_value,y='ICC',data=icc_data,hue='Group',palette='winter_r',kind='bar',col='Bands',col_wrap=4,legend=False)
+    ax.fig.suptitle(title)
+    ax.add_legend(loc='upper center',bbox_to_anchor=(.5,0.94),ncol=2)
+    ax.fig.subplots_adjust(top=0.829,bottom=0.133, right=0.936,left=0.062, hspace=0.143, wspace=0.11) # adjust the Figure in rp
+    ax.set(xlabel=None)
+    ax.set(ylabel=None)
+    ax.fig.text(0.5, 0.07, x_value, ha='center', va='center')
+    ax.fig.text(0.03, 0.5,  'ICC', ha='center', va='center',rotation='vertical')
+    if save==True:
+        plt.savefig('Reliability\ICC_Graphics\ICC_Comparacióngrupos_{tipo}.png'.format(tipo=x_value))
+        plt.close()
+    if plot:
+        plt.show()
+
+def plot_ICC_nG(icc_data,x_value,title,kind,plot=False,save=False):
+    sns.set(font_scale = 0.9)
+    sns.set_theme(style="white")
+    ax=sns.catplot(x=x_value,y='ICC',data=icc_data,hue='Group',palette='winter_r',kind=kind,legend=False)
+    ax.fig.suptitle(title)
+    ax.add_legend(loc='upper center',bbox_to_anchor=(.5,0.94),ncol=2,title='Group')
+    ax.fig.subplots_adjust(top=0.829,bottom=0.168, right=0.950,left=0.107, hspace=0.143, wspace=0.11)# adjust the Figure in rp
+    ax.set(xlabel=None)
+    ax.set(ylabel=None)
+    ax.fig.text(0.5, 0.07, x_value, ha='center', va='center')
+    ax.fig.text(0.03, 0.5,  'ICC', ha='center', va='center',rotation='vertical')
+    if save==True:
+        plt.savefig('Reliability\ICC_Graphics\ICC_Comparacióngrupos_sinsepararbandas_{tipo}.png'.format(tipo=x_value))
+        plt.close()
+    if plot:
+        plt.show()
+
+
+def barplot_icc_bandsx(icc_data,x_value, title,plot=False,save=False):
+    sns.set(font_scale = 0.9)
+    sns.set_theme(style="white")
+    ax=sns.catplot(x='Bands',y='ICC',data=icc_data,hue='Group',palette='winter_r',kind='bar',legend=False)
+    ax.fig.suptitle(title)
+    ax.add_legend(loc='upper center',bbox_to_anchor=(.5,0.94),ncol=2, title='Group')
+    ax.fig.subplots_adjust(top=0.829,bottom=0.168, right=0.950,left=0.107, hspace=0.143, wspace=0.11) # adjust the Figure in rp
+    ax.set(xlabel=None)
+    ax.set(ylabel=None)
+    ax.fig.text(0.5, 0.07, 'Bands', ha='center', va='center')
+    ax.fig.text(0.03, 0.5,  'ICC', ha='center', va='center',rotation='vertical')
+    if save==True:
+        plt.savefig('Reliability\ICC_Graphics\ICC_Comparacióngrupos_bandasx_{tipo}.png'.format(tipo=x_value))
+        plt.close()
+    if plot:
+        plt.show()
+  
+    
+
+barplot_icc_comp_nG(icc_data_Roi[icc_data_Roi['Stage']=='Normalized data'],'Roi','ICC3k for Rois in frequency bands',plot=True,save=True)
+barplot_icc_comp_nG(icc_data_Comp[icc_data_Comp['Stage']=='Normalized data'],'Components','ICC3k for IC in frequency bands', plot=True,save=True)
+
+barplot_icc_bandsx(icc_data_Roi[icc_data_Roi['Stage']=='Normalized data'],'Roi','ICC3k for ROIs in frequency bands',plot=True,save=True)
+barplot_icc_bandsx(icc_data_Comp[icc_data_Comp['Stage']=='Normalized data'],'Components','ICC3k for IC in frequency bands',plot=True,save=True)
+
+plot_ICC_nG(icc_data_Roi[icc_data_Roi['Stage']=='Normalized data'],'Roi','ICC3k for ROIs','bar',plot=True,save=True)
+plot_ICC_nG(icc_data_Comp[icc_data_Comp['Stage']=='Normalized data'],'Components','ICC3k for ICs','bar',plot=True,save=True)
+
+barplot_icc_nB_1G(icc_data_Roi,'Roi','G2',plot=True,save=True)
+barplot_icc_nB_1G(icc_data_Roi,'Roi','CTR',plot=True,save=True)
+barplot_icc_nB_1G(icc_data_Comp,'Components','G2',plot=True,save=True)
+barplot_icc_nB_1G(icc_data_Comp,'Components','CTR',plot=True,save=True)
+
+def icc_mean(data):
+    Stage=data['Stage'].unique()
+    bands=data['Bands'].unique()
+    for st in Stage:
+        print(st+':')
+        d_stage=data[data['Stage']==st]
+        print('Promedio general: '+str(d_stage['ICC'].mean())+' ± ' +str(d_stage['ICC'].std())+'\n')
+        for band in bands:
+            d_band=d_stage[d_stage['Bands']==band]
+            print('Promedio '+band+': '+str(d_band['ICC'].mean())+' ± ' +str(d_band['ICC'].std()))
+        print('\n')
+
+print('Promedios por bandas de ROIs\n')
+icc_mean(icc_data_Roi)
+print('Promedios por bandas de Componentes\n')
+icc_mean(icc_data_Comp)
---
+++ Reliability/Statistical_analysis.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pingouin as pg
+from scipy import stats
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
+
+def graphic_normality_box(data,x_value,y_value,title):
+    fig, ax = plt.subplots(1, 1, figsize=(8, 4))
+    #plt.title(title)
+    sns.boxplot(x=x_value, y=y_value, data=data, ax=ax)
+    sns.swarmplot(x=x_value, y=y_value, data=data, color='black', alpha = 0.5, ax=ax);
+    plt.show()
+
+def normality_N_groups_Shapiro(data,dv,group,method='shapiro'):
+    '''
+    data: pandas.DataFrame, series, list or 1D np.array
+    Iterable. Can be either a single list, 1D numpy array, or a wide- or long-format pandas dataframe.
+
+    dv: vstr (are de values)
+    Dependent variable (only when data is a long-format dataframe).
+
+    group:str
+    Grouping variable (only when data is a long-format dataframe).
+    '''
+    pg.normality(data=data, dv=dv, group=group,method=method)
+
+
+icc_data_Roi=pd.read_csv(r'C:\Users\valec\Documents\JI\Codigos\Data_analysis_ML_Harmonization_Proyect\Reproducibilidad\ICC_values_csv\icc_values_ROIS_G2-CTR.csv',sep=';')
+icc_data_Comp=pd.read_csv(r'C:\Users\valec\Documents\JI\Codigos\Data_analysis_ML_Harmonization_Proyect\Reproducibilidad\ICC_values_csv\icc_values_Components_G2-CTR.csv',sep=';')
+
+
+icc_data_Roi=icc_data_Roi[icc_data_Roi['Stage']=='Normalized data']
+icc_data_Comp=icc_data_Comp[icc_data_Comp['Stage']=='Normalized data']
+
+# graphic_normality_box(icc_data_Roi,'Group','ICC','ICC por grupos y Rois')
+# graphic_normality_box(icc_data_Comp,'Group','ICC','ICC por grupos y Componentes')
+
+##Como anova diferencias entre grupos pero no parametrica
+#Analsis de diferencias grupales por bandas
+# bands=icc_data_Comp['Bands'].unique()
+# print('Componentes')
+# for i in bands:
+#     print(i)
+#     print(pg.pairwise_gameshowell(data=icc_data_Comp[icc_data_Comp['Bands']==i], dv='ICC', between='Group'))
+
+# print('\nRois')
+# for i in bands:
+#     print(i)
+#     print(pg.pairwise_gameshowell(data=icc_data_Roi[icc_data_Roi['Bands']==i], dv='ICC', between='Group'))
+
+#Analsis de diferencias grupales por cada uno d elos ICC y rois
+print('\nComparaciones por componente y Roi')
+c=icc_data_Comp['Components'].unique()
+print('\nComponentes')
+for i in c:
+    print(i)
+    print(pg.pairwise_gameshowell(data=icc_data_Comp[icc_data_Comp['Components']==i], dv='ICC', between='Group'))
+
+print('\nRois')
+r=icc_data_Roi['Roi'].unique()
+for i in r:
+    print(i)
+    print(pg.pairwise_gameshowell(data=icc_data_Roi[icc_data_Roi['Roi']==i], dv='ICC', between='Group'))
